044613984510f60951386cf207ce8251e562ef2f
momo <82857794+orangecatmo@users.noreply.github.com>
2022-06-10 04:00:41 +0900

    Add files via upload

--- js/date.js
+++ js/date.js
@@ -26,18 +26,85 @@ function addZero(m) {
 }
 $(function() {
 
-    var arr = ['あなたはこの世界で一番無二です', '好きなことができるたけていい', '絶望している暇あったら、うまいもの食べて寝るかな', '落胆するな'];
-    var item = arr[Math.floor(Math.random() * arr.length)];
-    $('.list_p').html(item);
+    ""
+
+    var arr = [
+        {
+            auspicious: "小吉",
+            sign: "あなたはこの世界で一番無二です"
+        },{
+            auspicious: "大吉",
+            sign: "好きなことができるたけていい"
+        },{
+            auspicious: "中吉",
+            sign: "絶望している暇あったら、うまいもの食べて寝るかな"
+        },{
+            auspicious: "小吉",
+            sign: "明るく生きていこうって、そう決めてた人のような気がするんだ"
+        },{
+            auspicious: "大吉",
+            sign: "そのまんまでいいんです"
+        },{
+            auspicious: "末吉",
+            sign: "先のこと考えてたら、何もできないですよ"
+        },{
+            auspicious: "大吉",
+            sign: "人が頑張ったのって、頑張って生きたのって、目に見えないかもしれないけど、心に残るんだと思います。"
+        },{
+            auspicious: "中吉",
+            sign: "人生には、三つ坂があるんですって、上り坂、下り坂、まさか。"
+        },{
+            auspicious: "小吉",
+            sign: "何をするかじゃなくて誰とするかだと思うけどな"
+        },{
+            auspicious: "大吉",
+            sign: "どんなに幸せそうに見えても 、見えないところで みんな悩んでる"
+        },{
+            auspicious: "小吉",
+            sign: "持ち物 あなたの笑顔"
+        },{
+            auspicious: "末吉",
+            sign: "考えるという行為は、人間に与えられた最大の楽しみだ"
+        },{
+            auspicious: "大吉",
+            sign: "人生短いだからね、好きなことしなさい"
+        },{
+            auspicious: "中吉",
+            sign: "頑張りましょう、次から次に、手に入れたいものが増えてくるんですから"
+        },{
+            auspicious: "大吉",
+            sign: "数学はいいわよ、迷いながらも、答えはひとつしかない"
+        },{
+            auspicious: "小吉",
+            sign: "ずっと それ信じてやってきたわけじゃん"
+        },{
+            auspicious: "中吉",
+            sign: "大事なものが後から遅れてくることもあるのよ。愛情だって生活だって"
+        },{
+            auspicious: "末吉",
+            sign: "落胆するな"
+        },{
+            auspicious: "小吉",
+            sign: "落胆するな"
+            
+        }];
+        
+
+    
+	let omikuji = ["大吉","吉","中吉","小吉","末吉","凶","大凶"];
+
 
     //动画
     $('.mian_back').click(function() {
+        var item = arr[Math.floor(Math.random() * arr.length)];
         $(this).animate({ opacity: 0 }, 1000, function() {
-            $('.mian_back').css('background-image', "url('img/T2.png')");
+            $('.mian_back').css('background-image', "none");
+            $('.arrow').html(item.sign)
+            $('.list_p').html(item.auspicious).css({"color": "#e9c000","fontSize":"2rem"})
             $('.mian_back').animate({ opacity: 1 }, 1000);
-
+            
         });
-        $('.main_list').css("backgroundColor", "yellow");
+        $('.main_list').css("backgroundColor", "#00629F");
 
 
     })
